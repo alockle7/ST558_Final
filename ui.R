d30f16395b9b7d80595efb68d993eb7ced25b9f6
Autumn Locklear <alockle7@ncsu.edu>
2023-12-12 11:57:22 -0500

    Data import steps

--- ui.R
+++ ui.R
@@ -10,9 +10,10 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
-#import data, save as R object and covert categorical variables to factors. 
-sleep_data <- read.csv("Sleep_health_and_lifestyle_dataset.csv")
+#import data. 
+sleep_data <- read.csv("https://raw.githubusercontent.com/alockle7/ST558_Final/main/Sleep_health_and_lifestyle_dataset.csv")
 
+#Save as R object and convert categorical variables to factors
 sleep_data$Gender <- as.factor(sleep_data$Gender)
 sleep_data$Occupation <- as.factor(sleep_data$Occupation)
 sleep_data$BMI.Category <-as.factor(sleep_data$BMI.Category)
